3ece9ccb1e86ec67c4b1be9c21133d234b9eccbf
Ulugbek Aripov <ulugbeks@gmail.com>
2018-12-12 04:52:27 -0500

    Added face detection to AWS Lambda using Rekognition. Compare faces to be added

--- fymUrlToS3.js
+++ fymUrlToS3.js
@@ -1,34 +1,20 @@
 const request = require('request-promise');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-const ImageAnalyser = require('./imageAnalyser');
+const rek = new AWS.Rekognition();
 
-const analyzeImage = function (fileName, callback) {
-    const s3Config = {
-        bucket: process.env.BUCKET_NAME,
-        imageName: fileName,
-    };
 
-    return ImageAnalyser
-        .getImageLabels(s3Config)
-        .then((labels) => {
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify({
-                    Labels: labels
-                }),
-            };
-            callback(null, response);
-        })
-        .catch((error) => {
-            callback(null, {
-                statusCode: error.statusCode || 501,
-                headers: {
-                    'Content-Type': 'text/plain'
-                },
-                body: error.message || 'Internal server error',
-            });
-        });
+module.exports.handler = (event, context, callback) => {
+    let fileName;
+    console.log('event', event);
+    const type = event.type;
+    const theURL = decodeURIComponent(event.imageUrl);
+    console.log('theURL = ', theURL);
+
+    console.log('type = ', type);
+    if (type === "1") putToS3FromURL('fromURL.jpg', theURL, callback);
+    if (type === "2") getImageDescription('fromLocal.jpg', callback);
+
 }
 
 const putToS3FromURL = function (fileName, theURL, callback) {
@@ -52,21 +38,97 @@ const putToS3FromURL = function (fileName, theURL, callback) {
                 } else {
                     console.log("success uploading to s3");
 
-                    analyzeImage(fileName, callback);
+                    getImageDescription(fileName, callback);
                 }
             });
         }
     });
 }
 
-module.exports.handler = (event, context, callback) => {
-    let fileName;
-    const type = event.type;
-    const theURL = decodeURIComponent(event.imageUrl);
-    console.log('theURL = ', theURL);
+const getImageDescription = function (fileName, callback) {
 
-    console.log('type = ', type);
-    if (type === "1") putToS3FromURL('fromURL.jpg', theURL, callback);
-    if (type === "2") analyzeImage('fromLocal.jpg', callback);
+    return new Promise((resolve, reject) => {
 
+
+        // console.log(`Analyzing file: https://s3.amazonaws.com/${params.Image.S3Object.Bucket}/${params.Image.S3Object.Name}`);
+
+        return getLabels(fileName)
+            .then((data) => {
+                return getFaceInfo(fileName, data);
+            })
+            .then((data) => {
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        Labels: data
+                    }),
+                };
+                callback(null, response);
+            })
+            .catch((error) => {
+                callback(null, {
+                    statusCode: error.statusCode || 501,
+                    headers: {
+                        'Content-Type': 'text/plain'
+                    },
+                    body: error.message || 'Internal server error',
+                });
+            });
+    });
 }
+
+const getLabels = function (fileName) {
+
+    const params = {
+        Image: {
+            S3Object: {
+                Bucket: process.env.BUCKET_NAME,
+                Name: fileName
+            },
+        },
+        MaxLabels: 20,
+        MinConfidence: 50,
+    };
+
+    return new Promise((resolve, reject) => {
+        rek.detectLabels(params, (err, data) => {
+            if (err) {
+                return reject(new Error(err));
+            }
+            console.log('Analysis labels:', data.Labels);
+            return resolve(data.Labels);
+        });
+    });
+}
+
+const getFaceInfo = function (fileName, prevData) {
+
+    const params = {
+        Image: {
+            S3Object: {
+                Bucket: process.env.BUCKET_NAME,
+                Name: fileName
+            },
+        },
+        "Attributes": [
+            "ALL"
+        ]
+    };
+
+    return new Promise((resolve, reject) => {
+        rek.detectFaces(params, (err, data) => {
+            if (err) {
+                return reject(new Error(err));
+            }
+            console.log('Analysis face: ', data.FaceDetails);
+
+            const rekogData = {
+                labelsData: prevData,
+                faceData: data.FaceDetails
+            }
+
+            return resolve(rekogData);
+        });
+    });
+}
+
